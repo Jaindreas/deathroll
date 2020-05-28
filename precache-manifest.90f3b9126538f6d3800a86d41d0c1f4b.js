34a6d4314a90da682d518a375104aa8c50eefdf1
James Maguire <jaindreas@gmail.com>
2020-05-28 21:32:12 +0100

    Updates

---
+++ precache-manifest.90f3b9126538f6d3800a86d41d0c1f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcea26bbcfc35a043f9c74683419aa0",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "374859ea957ddb8fac62",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f05345fa3e3d8709d349",
+    "url": "/my-app/static/js/2.884bb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.884bb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374859ea957ddb8fac62",
+    "url": "/my-app/static/js/main.45498cad.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
