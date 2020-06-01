5166aaea841e9df236669473265a92fe2f5d2f60
James Maguire <jaindreas@gmail.com>
2020-06-01 20:55:12 +0100

    Added Winner Functionality. Passed Function to setState to allow real time update

--- src/Components/NumberGenerator.js
+++ src/Components/NumberGenerator.js
@@ -2,18 +2,44 @@ import React from 'react';
 
 class NumberGenerator extends React.Component {
   state = {
-      userScore : 0 
+      userScores : [
+        {score: 0},
+        {score: 0}
+      ],
+      winner: "No Winner "
   }
+
     // Generate Random Number Method
     generateRandomNumber = () => {
         let randomNumber = Math.round(Math.random() * 100);
-        this.setState({userScore: randomNumber});   
+        let randomNumber2 = Math.round(Math.random() * 100);
+        this.setState({
+            userScores: [
+            {score: randomNumber},
+            {score: randomNumber2} 
+            ]
+        }, () => this.decideWinner())
+        
     }
 
+     decideWinner = () => {
+        if ((this.state.userScores[0].score) > (this.state.userScores[1].score)) {
+            this.setState({
+                winner: "Player 1"
+            })
+        } else {
+            this.setState({
+                winner: "Player 2"
+            })
+        }
+    }
     render () {
         return (
-            <div class = "numberGenerator">
-            <h1>{this.state.userScore}</h1>
+            <div className = "numberGenerator">
+            <h1>{this.state.userScores[0].score}</h1>
+            <h1>{this.state.userScores[1].score}</h1>
+            <h2>{this.state.winner + " Wins!"}</h2>
+            <button onClick = {this.generateRandomNumber}> Death Roll </button>
             <button onClick = {this.generateRandomNumber}> Death Roll </button>
             </div>
         )
