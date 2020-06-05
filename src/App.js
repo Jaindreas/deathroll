9b5454b145e2673e81bf6f67e5cd5493adcccd1b
James Maguire <jaindreas@gmail.com>
2020-06-05 17:06:28 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -7,67 +7,151 @@ class App extends React.Component {
    state = {
       score1: 0,
       score2: 0,
-      winner: "Not Declared",
+      scoreboardplayer1: [],
+      scoreboardplayer2: [],
+      gamemessage: "Who goes first? Roll!",
+      playermessage: "",
       buttonToggle1: "enabled",
-      buttonToggle2: '',
-      roll: 100
+      roll: 100,
+      hasCheckedWhoGoesFirst: false
+  } 
+
+  gamesSequence  = () => {
+    
+    if (this.state.hasCheckedWhoGoesFirst) {
+      console.log(this.state.score1)
+      console.log(this.state.score2)
+      this.decideWinner()
+      this.addScoreBoard();
+      setTimeout(() => this.defaultMessage(), 1500)
+    } else {
+    this.whoGoesFirst()
+    setTimeout(() => this.defaultMessage(), 1500)
+    }
   }
+
+  whoGoesFirst = () => {
+    switch (true) {
+      case this.state.score1 < this.state.score2: 
+        this.setState({
+        gamemessage: "Player 1 Goes first"
+      });
+      break;
+      case this.state.score2 < this.state.score1:
+         this.setState({
+          gamemessage: "Computer Goes first"
+         });
+       break;
+      default: 
+      this.setState({
+        gamemessage: "Draw Game"
+      })
+   }
+  }
+
   generateRandomNumber = (player, roll) => {
     let randomNumber = Math.round(Math.random() * (roll - 1) + 1);
-    console.log(roll);
-    if (player === 1) {
+    let randomNumber2 = Math.round(Math.random() * (roll - 1) + 1);
     this.setState({
       score1: randomNumber,
-        buttonToggle1: '',
-        buttonToggle2: "enabled"
-    })  
-  } else if (player === 2) {
-    this.setState({
-      score2: randomNumber,
-        buttonToggle2: '' 
-    } , () => this.decideWinner())
-  }
+      score2: randomNumber2,
+        //buttonToggle1: ''
+    }, () => this.gamesSequence()) 
 }
- decideWinner = () => {
-  if (this.state.score1 === 1) {
+
+decideWinner = () => {
+ 
+  switch (true) {
+    case this.state.score1 > this.state.score2:
     this.setState({
-      winner: "Player 1 Loses"
+      gamemessage: "Computer Rolls First",
+      playermessage: "Next role is out of " + this.state.score2,
+      roll: this.state.score2
+  })
+  break;
+    case ((this.state.score2) > (this.state.score1)):
+    this.setState({
+      gamemessage: "Player 1 Rolls First",
+      playermessage: "Next role is out of " + this.state.score1,
+      roll: this.state.score1
+  })
+  break;
+    case this.state.score1 === 1: 
+    this.setState({
+      playermessage: "Player 1 Loses"
     })
-  } else if ((this.state.score1) > (this.state.score2)) {
-       this.setState({
-           winner: "Next role is out of " + this.state.score2,
-           buttonToggle1: "enabled",
-           buttonToggle2: "enabled",
-           roll: this.state.score2
-       })
-   } else if ((this.state.score2) > (this.state.score1)) {
-      this.setState({
-           winner: "Next role is out of " + this.state.score1,
-           buttonToggle1: "enabled",
-           buttonToggle2: "enabled",
-           roll: this.state.score1
-      })
-      
-   } 
+  break;
+    case this.state.score2 === 1: 
+    this.setState({
+      playermessage: "Computer Loses"
+    })
+  break;
+    default:
+    this.setState({
+      gamemessage: "Computer Encountered an Error"
+    })  
+  }
  }
 
- 
 
+ defaultMessage = () => {
+  this.setState({
+    score1: [0],
+    score2: 0,
+    //gamemessage: "Roll!",
+    hasCheckedWhoGoesFirst: true
+  });
+}
+
+addScoreBoard = () => {
+  let newArray = this.state.scoreboardplayer1.concat(this.state.score1 + ",")
+  let newArray2 = this.state.scoreboardplayer2.concat(this.state.score2 + ",")
+  console.log(newArray)
+  this.setState({
+    scoreboardplayer1: [newArray],
+    scoreboardplayer2: [newArray2]
+  })
+}
+
+resetGame = () => {
+  this.setState({
+    score1: [0],
+    score2: [0],
+    scoreboardplayer1: [],
+    scoreboardplayer2: [],
+    gamemessage: "Who goes first? Roll!",
+    buttonToggle1: "enabled",
+    roll: 100,
+    hasCheckedWhoGoesFirst: false
+})
+
+}
 
   render() {
-
     const style = {
       color: "royalblue"
     }
   return (
     <div className="App">
       <h1> <u> Death Roll</u> </h1>
+      <h4> {this.state.gamemessage}</h4>
+      <h3> {this.state.playermessage} </h3>
+      
+      <h4> Computer </h4>
+      <h1 style = {style}>{this.state.score2}</h1>
+     
       <h4> Player 1 </h4>
       <NumberGenerator style = {style} userscores = {this.state.score1} generate = {() => this.generateRandomNumber(1, this.state.roll)} buttontoggle = {this.state.buttonToggle1}  />
-      <h4> Player 2 </h4>
-      <NumberGenerator style = {style} userscores = {this.state.score2} generate = {() => this.generateRandomNumber(2, this.state.roll)} buttontoggle = {this.state.buttonToggle2}  /> 
-      <ScoreCard number = {this.state.score1} />
-      <h4> {this.state.winner}</h4>
+  
+     <button onClick = {this.resetGame}>Reset Game</button>
+      
+      
+      {/* <NumberGenerator style = {style} userscores = {this.state.score2} generate = {() => this.generateRandomNumber(2, this.state.roll)} buttontoggle = {this.state.buttonToggle2}  />  */}
+      <div className = "scorecard">
+      <ScoreCard scoreplayer1 = {this.state.scoreboardplayer1} scoreplayer2 = {this.state.scoreboardplayer2}  />
+    
+      </div>
+     
     </div>
   );
   }
--- src/Components/ScoreCard.js
+++ src/Components/ScoreCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './NumberGenerator';
+import './ScoreCard.css';
 
 
 class ScoreCard extends React.Component  {
     render() {
     return (
-      <div>
-       <h3>Scores: {this.props.number} </h3>
+      <div className = "scoreboard">
+       <h3><u>Scores</u> </h3>
+       <h4> Player 1 </h4> 
+       <ol>
+        <li>{this.props.scoreplayer1} </li>
+       </ol>
+       <h4> Computer  </h4>
+       <ol>
+        <li>{this.props.scoreplayer2} </li>
+       </ol>
       </div>
     )
    }
